c82fb170b523b6910a7e1aeb12624f17c00efc79
mkulesh <mikhail.kulesh@gmail.com>
2018-12-19 18:17:18 +0100

    Updated autotest

--- autotest/run-session.sh
+++ autotest/run-session.sh
@@ -6,9 +6,9 @@ ${ANDROID_HOME}/tools/emulator -list-avds
 
 echo APK to be tested: ${1}
 
-./run-emulator.sh android_4.0.3 ${1}
+./run-emulator.sh android_4.0.4 ${1}
 ./run-emulator.sh android_4.1.2 ${1}
-#./run-emulator.sh android_4.2.2 ${1} - Skipped: SD does not more work in the new emulator
+./run-emulator.sh android_4.2.2 ${1}
 ./run-emulator.sh android_4.3.1 ${1}
 ./run-emulator.sh android_4.4.2 ${1}
 ./run-emulator.sh android_5.0.1 ${1}
@@ -17,6 +17,8 @@ echo APK to be tested: ${1}
 ./run-emulator.sh android_7.0.0 ${1} -g
 ./run-emulator.sh android_7.1.1 ${1} -g
 ./run-emulator.sh android_8.0.0 ${1} -g
+./run-emulator.sh android_8.1.0 ${1} -g
+./run-emulator.sh android_9.0.0 ${1} -g
 
 firefox *.html
 
