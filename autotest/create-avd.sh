afebc51e5a3c7d8225d7e90c1aee40be8c5ce6da
Mikhail Kulesh <mikhail.kulesh@gmail.com>
2024-03-24 21:44:32 +0100

    #133 Adapt to Android 14: updated AVD creation and TA scrips
    (cherry picked from commit d7026e8fce2ae9bbcd0a8b7fc4ec59878aad573a)

--- autotest/create-avd.sh
+++ autotest/create-avd.sh
@@ -1,33 +1,43 @@
 #!/bin/sh
 
-TOOLS_PATH=/work/android/sdk/tools
+TOOLS_PATH=/work/android/sdk/cmdline-tools/latest/bin
 AVDS_PATH=/work/android/avds
+DEVICE_NAME="pixel_xl"
 
 createAVD()
 {
 echo ""
 echo "----------------------------------------------------------------------------------"
 echo "Creating $1($2)"
-echo 'no' | ${TOOLS_PATH}/bin/avdmanager create avd --name $1 --path ${AVDS_PATH}/$1 --sdcard 200M --force --package $2
-# after creation, manually add in each .ini file the following parameters:
-cat device.cfg >> ${AVDS_PATH}/$1/config.ini
+${TOOLS_PATH}/avdmanager create avd --name $1 --path ${AVDS_PATH}/$1 --sdcard 256M --force --package $2 --device ${DEVICE_NAME}
+echo "hw.ramSize=2048" >> ${AVDS_PATH}/$1/config.ini
+echo "skin.path=_no_skin" >> ${AVDS_PATH}/$1/config.ini
+sed -i 's/showDeviceFrame=yes/showDeviceFrame=no/g' ${AVDS_PATH}/$1/config.ini
+sed -i 's/hw.keyboard=no/hw.keyboard=yes/g' ${AVDS_PATH}/$1/config.ini
+sed -i 's/hw.gpu.enabled=no/hw.gpu.enabled=yes/g' ${AVDS_PATH}/$1/config.ini
+sed -i 's/hw.gpu.mode=auto/hw.gpu.mode=host/g' ${AVDS_PATH}/$1/config.ini
+sed -i 's/fastboot.forceColdBoot=no/fastboot.forceColdBoot=yes/g' ${AVDS_PATH}/$1/config.ini
+sed -i 's/fastboot.forceFastBoot=yes/fastboot.forceFastBoot=no/g' ${AVDS_PATH}/$1/config.ini
 }
 
-createAVD android_4.0.4 'system-images;android-15;google_apis;x86'
-createAVD android_4.1.2 'system-images;android-16;google_apis;x86'
-createAVD android_4.2.2 'system-images;android-17;google_apis;x86'
-createAVD android_4.3.1 'system-images;android-18;google_apis;x86'
-createAVD android_4.4.2 'system-images;android-19;google_apis;x86'
-createAVD android_5.0.1 'system-images;android-21;default;x86_64'
-createAVD android_5.1.1 'system-images;android-22;default;x86_64'
-createAVD android_6.0.0 'system-images;android-23;default;x86_64'
-createAVD android_7.0.0 'system-images;android-24;default;x86_64'
-createAVD android_7.1.1 'system-images;android-25;google_apis;x86'
-createAVD android_8.0.0 'system-images;android-26;google_apis;x86'
-createAVD android_8.1.0 'system-images;android-27;google_apis;x86'
-createAVD android_9.0.0 'system-images;android-28;google_apis;x86'
-
-echo ""
-echo "----------------------------------------------------------------------------------"
-${TOOLS_PATH}/emulator -list-avds
+mkdir -p ${AVDS_PATH}
+createAVD android_4.0.4  'system-images;android-15;google_apis;x86'
+createAVD android_4.1.2  'system-images;android-16;google_apis;x86'
+createAVD android_4.2.2  'system-images;android-17;google_apis;x86'
+createAVD android_4.3.1  'system-images;android-18;google_apis;x86'
+createAVD android_4.4.2  'system-images;android-19;google_apis;x86'
+createAVD android_5.0.2  'system-images;android-21;google_apis;x86_64'
+createAVD android_5.1.1  'system-images;android-22;google_apis;x86_64'
+createAVD android_6.0.0  'system-images;android-23;google_apis;x86_64'
+createAVD android_7.0.0  'system-images;android-24;google_apis;x86_64'
+createAVD android_7.1.1  'system-images;android-25;google_apis;x86_64'
+createAVD android_8.0.0  'system-images;android-26;google_apis;x86_64'
+createAVD android_8.1.0  'system-images;android-27;google_apis;x86'
+createAVD android_9.0.0  'system-images;android-28;google_apis;x86_64'
+createAVD android_10.0.0 'system-images;android-29;google_apis;x86_64'
+createAVD android_11.0.0 'system-images;android-30;google_apis;x86_64'
+createAVD android_12.0.0 'system-images;android-31;google_apis;x86_64'
+createAVD android_12.1.0 'system-images;android-32;google_apis;x86_64'
+createAVD android_13.0.0 'system-images;android-33;google_apis;x86_64'
+createAVD android_14.0.0 'system-images;android-34;google_apis;x86_64'
 
