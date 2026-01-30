bd40d65a169d8681e2fd4844cc05802fb14eb424
Mikhail Kulesh <mikhail.kulesh@gmail.com>
2026-01-30 23:36:48 +0100

    #146 Automatic determination of plot-boundaries fails for negative interval endpoints: fixed in premium version
    (cherry picked from commit 61bc18527d6920d0e6cc09e249c38d2432486953)

--- app/src/main/java/com/mkulesh/micromath/math/CalculatedValue.java
+++ app/src/main/java/com/mkulesh/micromath/math/CalculatedValue.java
@@ -27,6 +27,7 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
 import java.util.List;
+import java.util.Locale;
 
 import androidx.annotation.NonNull;
 
@@ -328,7 +329,10 @@ public class CalculatedValue
             }
             else
             {
-                return addPlus ? ("+" + roundV) : Double.toString(roundV);
+                // #146: By formatting using Locale.US, we are explicitly requesting
+                // a set of symbols that are standard and internationally recognized for
+                // machine parsing and consistent display
+                return addPlus ? ("+" + roundV) : String.format(Locale.US, "%s", roundV);
             }
         }
     }
