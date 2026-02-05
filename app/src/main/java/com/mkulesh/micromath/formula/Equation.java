6d299086fa42155fc3a7eb22fc983b9ae074aed5
Mikhail Kulesh <mikhail.kulesh@gmail.com>
2026-02-05 19:45:48 +0100

    #147 Interval defined from another interval gives inconsistent plotting behaviour: fixed
    (cherry picked from commit 1b01e3d1bfcf5dfa2869ed8ddcb9a79ac4174713)

--- app/src/main/java/com/mkulesh/micromath/formula/Equation.java
+++ app/src/main/java/com/mkulesh/micromath/formula/Equation.java
@@ -110,13 +110,17 @@ public class Equation extends CalculationResult implements ArgumentHolderIf, Cal
         {
             n += getArguments().toString();
         }
-        if (isIntervalTerm())
+        if (isIntervalEq(true))
         {
-            n += ": interval";
+            n += "; interval";
+        }
+        if (isDerivedInterval())
+        {
+            n += "(derived)";
         }
         else if (isArray())
         {
-            n += ": array";
+            n += "; array";
         }
         return "Formula " + getBaseType().toString() + "(Id: " + getId() + ", Name: " + n + ")";
     }
@@ -175,7 +179,7 @@ public class Equation extends CalculationResult implements ArgumentHolderIf, Cal
                 break;
             }
             // check that the equation is an interval
-            if (isIntervalTerm())
+            if (isIntervalEq(true))
             {
                 arrayResult = new EquationArrayResult(this, null);
                 break;
@@ -358,9 +362,26 @@ public class Equation extends CalculationResult implements ArgumentHolderIf, Cal
         {
             return;
         }
-        if (isIntervalTerm())
+        if (isIntervalEq(false))
         {
             ((Intervals) rightTerm.getTerm()).getInterval(arrayResult, thread);
+        }
+        else if (isDerivedInterval() && getDirectIntervals().size() == 1)
+        {
+            final Equation arg = getDirectIntervals().get(0);
+            final CalculatedValue[] argValues = new CalculatedValue[1];
+            argValues[0] = new CalculatedValue();
+            final CalculatedValue[] xValues = arg.getInterval();
+            if (xValues != null)
+            {
+                arrayResult.resize1D(xValues.length);
+                for (int xIndex = 0; xIndex < xValues.length; xIndex++)
+                {
+                    argValues[0].assign(xValues[xIndex]);
+                    arg.setArgumentValues(argValues);
+                    rightTerm.getValue(thread, arrayResult.getValue1D(xIndex));
+                }
+            }
         }
         else
         {
@@ -464,12 +485,22 @@ public class Equation extends CalculationResult implements ArgumentHolderIf, Cal
      */
     public boolean isInterval()
     {
-        return isArray() || isIntervalTerm();
+        return isArray() || isIntervalEq(true);
     }
 
-    private boolean isIntervalTerm()
+    private boolean isDerivedInterval()
     {
-        return (rightTerm.getTerm() != null && rightTerm.getTerm() instanceof Intervals);
+        final ArrayList<String> args = getArguments();
+        return getDirectIntervals().size() == 1 && (args == null || args.isEmpty());
+    }
+
+    private boolean isIntervalEq(boolean considerDerived)
+    {
+        if (rightTerm.getTerm() != null && rightTerm.getTerm() instanceof Intervals)
+        {
+            return true;
+        }
+        return considerDerived && isDerivedInterval();
     }
 
     /**
